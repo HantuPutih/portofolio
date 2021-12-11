9ce937c9e359b3cd3e7d310163a8623a6d7dd542
Farid Juliano <thanatos.juliano@gmail.com>
2021-12-11 11:51:59 +0700

    11/12/21

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,35 @@
 import "./intro.scss";
-
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 export default function Intro() {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Front-End Engineer"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
-      Intro
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I am</h2>
+          <h1>Farid Juliano</h1>
+          <h3>
+            Fulltime <span ref={textRef}></span>
+          </h3>
+          <a href="#portofolio">
+            <img src="assets/down.png" alt="" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
