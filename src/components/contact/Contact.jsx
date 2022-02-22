249083bbbfa67a8a4e500b6bbfae7bc1b395db7a
Farid Juliano <thanatos.juliano@gmail.com>
2022-02-22 17:42:38 +0700

    optimize 22/2/22

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,39 +1,63 @@
 import "./contact.scss";
-import {useState} from "react";
-import shake from '../../assets/shake.svg'
-import {  Mail }  from "@material-ui/icons"
-import PhoneIcon from '@mui/icons-material/Phone';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
+// import {useState} from "react";
+import shake from "../../assets/shake.svg";
+import { Mail } from "@material-ui/icons";
+import PhoneIcon from "@mui/icons-material/Phone";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export default function Contact() {
-  const [message, setMessage] = useState(false);
-
-  const handleSubmit = (e) => {
-      e.preventDefault();
-    setMessage(true)
-  }
-
   return (
     <div className="contact" id="contact">
       <div className="left">
-        <img src={shake} alt=""/>
+        <LazyLoadImage src={shake} alt="" />
       </div>
       <div className="right">
         <h2>Contact</h2>
 
-        <h4><LinkedInIcon/> &nbsp; <a href="https://www.linkedin.com/in/farid-juliano/ "rel="noreferrer"  target="_blank">LinkedIn</a></h4>
-        <h4><GitHubIcon/> &nbsp; <a href="https://github.com/HantuPutih"  rel="noreferrer" target="_blank">HantuPutih</a></h4>
-        <h4><Mail/>&nbsp; <a href="mailto:thanatos.juliano@gmail.com" rel="noreferrer"> thanatos.juliano@gmail.com </a> </h4>
-        <h4><PhoneIcon/> &nbsp; <a href="https://api.whatsapp.com/send?phone=6287889278098" rel="noreferrer" target="_blank">+62 987-8927-8098</a> </h4>
-        <h4><LocationOnIcon/> &nbsp; Jakarta Selatan, Indonesia</h4>
-        {/*<form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email"/>
-          <textarea placeholder="Message"></textarea>
-          <button type="submit">Send</button>
-          {message && <span> Thanks, I will reply ASAP :)</span>}
-        </form>*/}
+        <h4>
+          <LinkedInIcon /> &nbsp;{" "}
+          <a
+            href="https://www.linkedin.com/in/farid-juliano/ "
+            rel="noreferrer"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+        </h4>
+        <h4>
+          <GitHubIcon /> &nbsp;{" "}
+          <a
+            href="https://github.com/HantuPutih"
+            rel="noreferrer"
+            target="_blank"
+          >
+            HantuPutih
+          </a>
+        </h4>
+        <h4>
+          <Mail />
+          &nbsp;{" "}
+          <a href="mailto:thanatos.juliano@gmail.com" rel="noreferrer">
+            {" "}
+            thanatos.juliano@gmail.com{" "}
+          </a>{" "}
+        </h4>
+        <h4>
+          <PhoneIcon /> &nbsp;{" "}
+          <a
+            href="https://api.whatsapp.com/send?phone=6287889278098"
+            rel="noreferrer"
+            target="_blank"
+          >
+            +62 987-8927-8098
+          </a>{" "}
+        </h4>
+        <h4>
+          <LocationOnIcon /> &nbsp; Jakarta Selatan, Indonesia
+        </h4>
       </div>
     </div>
   );
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,70 +1,74 @@
 import "./works.scss";
-import {useState} from "react";
-import arrow from '../../assets/arrow.png'
-import mobile from '../../assets/mobile.png'
-import globe from '../../assets/globe.png'
-import writing from '../../assets/writing.png'
-
+import { useState } from "react";
+import arrow from "../../assets/arrow.png";
+import mobile from "../../assets/mobile.png";
+import globe from "../../assets/globe.png";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 export default function Works() {
   const [currentSlide, setCurrentSlide] = useState(0);
   const data = [
     {
-    id: "1",
-    icon: globe,
-    title: "Front End Development",
-    desc:
-      "I can develop your website front end using Vue or React",
-    img:
-      "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
-
+      id: "1",
+      icon: globe,
+      title: "Front End Development",
+      desc: "I can develop your website front end using Vue or React",
+      img: "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
     },
     {
       id: "2",
       icon: mobile,
       title: "mobile Development",
-      desc:
-        "I can develop your mobile app using React Native",
-      img:
-        "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
-
+      desc: "I can develop your mobile app using React Native",
+      img: "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
     },
-  ]
+  ];
 
-  const handleClick =(way) => {
-    way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 1) :
-      setCurrentSlide(currentSlide <data.length - 1 ?currentSlide + 1 : 0 )
-  }
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 1)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
 
   return (
     <div className="works" id="works">
-      <div className="slider" style={{transform: `translateX(-${currentSlide * 100}vw)`}}>
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         {data.map((d, i) => (
-
-        <div key={i} className="container">
-          <div className="item">
-            <div className="left">
-              <div className="leftContainer">
-                <div className="imgContainer">
-                  <img src={d.icon} alt="phone"/>
+          <div key={i} className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={d.icon} alt="phone" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  <span>Projects</span>
                 </div>
-                <h2>{d.title}</h2>
-                <p>{d.desc}
-                </p>
-                <span>Projects</span>
+              </div>
+              <div className="right">
+                <img src={d.img} alt="" />
               </div>
             </div>
-            <div className="right">
-              <img
-                src={d.img}
-                alt=""/>
-            </div>
           </div>
-        </div>
-          ))
-        }
+        ))}
       </div>
-      <img src={arrow} className="arrow left" alt="" onClick={() => handleClick("left") }/>
-      <img src={arrow} className="arrow right" alt="" onClick={() => handleClick("right") }/>
+      <LazyLoadImage
+        src={arrow}
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <LazyLoadImage
+        src={arrow}
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick("right")}
+      />
+      {/* <img src={arrow} className="arrow left" alt="" onClick={() => handleClick("left") }/>
+      <img src={arrow} className="arrow right" alt="" onClick={() => handleClick("right") }/> */}
     </div>
   );
 }
